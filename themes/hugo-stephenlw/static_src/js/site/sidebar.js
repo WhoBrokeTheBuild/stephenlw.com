d94bdc1cb2843913671c9dfb984f7caf8696ef50
Stephen Lane-Walsh <slanewalsh@mywebgrocer.com>
2016-03-28 14:04:49 -0400

    Added "Creating a Hugo Theme" post Added a sidebar to the blog post page Fixed glyphicons

---
+++ themes/hugo-stephenlw/static_src/js/site/sidebar.js
@@ -0,0 +1,12 @@
+
+(function($){
+
+    $(function(){
+
+        $('.js-sidebar-toggle').click(function(){
+            $(this).parent('.js-sidebar').toggleClass('shown');
+        });
+
+    });
+
+}(jQuery));
